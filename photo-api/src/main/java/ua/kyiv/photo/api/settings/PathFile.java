28b4948f9050931f1c562e3218ef455142af87ff
firsachi <firsachi@firsbook>
2019-03-05 12:02:20 +0200

    Create PathFile

---
+++ photo-api/src/main/java/ua/kyiv/photo/api/settings/PathFile.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ua.kyiv.photo.api.settings;
+
+/**
+ * @author firsachi
+ *
+ */
+public class PathFile {
+	
+	public static final String PATCH_PARAMETRS = "/home/scanner//%s//%s.jpg";
+	
+	public static final String PATCH_TWO_PARAMETRS = "/home/scanner//%s//%s//%s.jpg";
+
+}
